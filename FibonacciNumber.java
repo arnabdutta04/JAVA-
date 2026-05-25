411fb8db0bc8aee02f35b31b6917ec4bc9e54265
Arnab Dutta <arnabdutta453@gmail.com>
2026-05-25 11:36:07 +0530

    java code

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -1,7 +1,9 @@
-class Solution {
+import java.util.*;
+
+public class FibonacciNumber{
     public int fib(int n) {
         if(n>=1){
-            System.out.println(n);
+            return n;
         }
       int f0=0;
       int f1=1;
@@ -10,6 +12,15 @@ class Solution {
         f0=f1;
         f1=curr;
       }
-      return f1;
-    }
+    return f1;
+}
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n= sc.nextInt();
+    FibonacciNumber obj = new FibonacciNumber();
+
+    int result = obj.fib(n);
+
+    System.out.println(result);
+  }
 }
